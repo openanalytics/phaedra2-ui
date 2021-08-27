100b313f1e13ab2d4b2d093af7b09688f152b394
Frederick Michielssen <fmichielssen@openanalytics.eu>
2021-08-27 13:30:51 +0200

    Prototyping plateview layout

--- src/components/views/PlateView.vue
+++ src/components/views/PlateView.vue
@@ -1,31 +1,63 @@
 <template>
-  <div class="title">
-    <h1>Plate {{plate.barcode}}</h1>
-  </div>
-  <div class="section">
-    <p>
-      <ul>
-        <li>ID: {{plate.id}}</li>
-        <li>Barcode: {{plate.barcode}}</li>
-        <li>Dimensions: {{plate.rows}} x {{plate.columns}}</li>
-        <li>Sequence: {{plate.sequence}}</li>
-      </ul>
-      <button type="button" class="btn btn-primary">Primary</button>
-    </p>
-  </div>
-  <div class="section">
-    <WellGrid :plate="plate"></WellGrid>
-  </div>
+  <q-card class="plate-header">
+    <q-card-section>
+      <div class="text-h6 text-primary">Plate {{plate.barcode}}</div>
+      <div class="row">
+        <div class="col-5">
+          <div class="row">
+            <div class="col-2 text-weight-bold">ID:</div><div class="col">{{plate.id}}</div>
+          </div>
+          <div class="row">
+            <div class="col-2 text-weight-bold">Barcode:</div><div class="col">{{plate.barcode}}</div>
+          </div>
+          <div class="row">
+            <div class="col-2 text-weight-bold">Dimensions:</div><div class="col">{{plate.rows}} x {{plate.columns}} ({{plate.rows * plate.columns}} wells)</div>
+          </div>
+          <div class="row">
+            <div class="col-2 text-weight-bold">Sequence:</div><div class="col">{{plate.sequence}}</div>
+          </div>
+          <div class="row">
+            <div class="col-2 text-weight-bold">Tags:</div>
+            <div class="col">
+              <q-chip color="green" text-color="white" label="Ref Plate" />
+              <q-chip color="orange" text-color="white" label="Experimental" />
+            </div>
+          </div>
+        </div>
+        <div class="col-5">
+          <div class="row">
+            <div class="col-2 text-weight-bold">Properties:</div>
+            <div class="col">
+
+            </div>
+          </div>
+        </div>
+        <div class="col-2">
+          <div class="row plate-button"><q-btn size="sm" rounded color="primary" label="Edit" /></div>
+          <div class="row plate-button"><q-btn size="sm" rounded color="primary" label="Move" /></div>
+          <div class="row plate-button"><q-btn size="sm" rounded color="primary" label="Delete" /></div>
+          <div class="row plate-button"><q-btn size="sm" rounded color="primary" icon="more_horiz" /></div>
+        </div>
+      </div>
+    </q-card-section>
+  </q-card>
+
+  <q-card class="plate-body">
+    <q-card-section>
+      <WellGrid :plate="plate"></WellGrid>
+    </q-card-section>
+  </q-card>
 </template>
 
 <style>
-  .title {
-    margin: 5px;
+  .plate-header {
+    margin: 10px;
   }
-  .section {
-    margin: 5px;
-    padding: 5px;
-    border: 1px solid black;
+  .plate-body {
+    margin: 10px;
+  }
+  .plate-button {
+    margin: 3px;
   }
 </style>
 
