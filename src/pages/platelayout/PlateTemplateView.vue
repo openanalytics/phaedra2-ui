afb5b0db4dd44f5966e9149a15df695b588553b2
smarien <simon.marien@openanalytics.eu>
2021-12-16 15:29:30 +0100

    Fix bugs + delete dialog

--- src/pages/platelayout/PlateTemplateView.vue
+++ src/pages/platelayout/PlateTemplateView.vue
@@ -1,32 +1,32 @@
 <template>
   <q-page class="oa-root-div" :style-fn="pageStyleFnForBreadcrumbs">
     <div class="q-pa-md" v-if="!editdialog">
-      <div class="text-h6 q-px-sm oa-section-title" v-if="!plate">
+      <div class="text-h6 q-px-sm oa-section-title" v-if="!plateTemplate">
         Loading plate...
       </div>
       <div v-else>
         <div class="row text-h6 items-center q-px-sm oa-section-title">
-          <q-icon name="view_module" class="q-mr-sm"/>
-          {{ plate.barcode }}
+          <q-icon name="border_outer" class="q-mr-sm"/>
+          {{ plateTemplate.name }}
         </div>
         <div class="row col-4 q-pa-lg oa-section-body">
           <div class="col col-4">
             <div class="row">
               <div class="col-3 text-weight-bold">ID:</div>
-              <div class="col">{{ plate.id }}</div>
+              <div class="col">{{ plateTemplate.id }}</div>
             </div>
             <div class="row">
               <div class="col-3 text-weight-bold">Dimensions:</div>
-              <div class="col">{{ plate.rows }} x {{ plate.columns }} ({{ plate.rows * plate.columns }} wells)</div>
+              <div class="col">{{ plateTemplate.rows }} x {{ plateTemplate.columns }} ({{ plateTemplate.rows * plateTemplate.columns }} wells)</div>
             </div>
             <div class="row">
               <div class="col-3 text-weight-bold">Description:</div>
-              <div class="col">{{ plate.description }}</div>
+              <div class="col">{{ plateTemplate.description }}</div>
             </div>
             <div class="row">
               <div class="col-3 text-weight-bold">Tags:</div>
               <div class="col">
-                <div class="tag-icon flex inline" v-for="tag in plate.tags" :key="tag.tag">
+                <div class="tag-icon flex inline" v-for="tag in plateTemplate.tags" :key="tag.tag">
                   <Tag :tagInfo="tag"></Tag>
                 </div>
               </div>
@@ -39,7 +39,7 @@
               <div class="col">
                 <q-table
                     dense
-                    :rows="plate.properties"
+                    :rows="plateTemplate.properties"
                     :columns="propertyColumns"
                     table-header-class="text-grey"
                     row-key="key"
@@ -72,9 +72,35 @@
         </div>
       </div>
     </div>
-    <div v-if="plate">
-      <well-grid :plate="plate" :gridType="layout"></well-grid>
+    <div v-if="plateTemplate">
+      <well-grid :plate="plateTemplate" :gridType="layout"></well-grid>
     </div>
+
+    <q-dialog v-model="deletedialog" persistent>
+      <q-card style="min-width: 30vw">
+        <q-card-section class="row text-h6 items-center full-width q-pa-sm bg-primary text-secondary">
+          <q-avatar icon="delete" color="primary" text-color="white"/> Delete Template
+        </q-card-section>
+        <q-card-section>
+          <div class="row">
+            <div class="col-10">
+              <span>Are you sure you want to delete the template <b>{{plateTemplate.name}}</b>?</span><br/>
+              <span>Type <span style="font-weight: bold">{{plateTemplate.name}}</span> and press the button to confirm:</span><br/>
+              <q-input dense v-model="plateTemplateName" autofocus/>
+              <br>
+              <span class="text-accent">WARNING: The template and associated data will be deleted!</span>
+            </div>
+          </div>
+        </q-card-section>
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat label="Cancel" v-close-popup/>
+          <router-link :to="{ name: 'dashboard'}" class="nav-link">
+            <q-btn label="Delete template" color="accent" v-if="plateTemplate.name==plateTemplateName" v-close-popup
+                   @click="deletePlateTemplate"/>
+          </router-link>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
 
   </q-page>
 </template>
@@ -109,67 +135,41 @@
 
 
 <script>
-import {computed, watch, ref} from 'vue'
+import {computed, ref} from 'vue'
 import {useStore} from 'vuex'
 
 import Tag from "@/components/tag/Tag";
 import WellGrid from "../plate/WellGrid";
+import {useRoute} from "vue-router";
 const propertyColumns = [
   {name: 'key', align: 'left', label: 'Name', field: 'key', sortable: true},
   {name: 'value', align: 'left', label: 'Value', field: 'value', sortable: true}
 ]
 
 export default {
-  name: 'Plate',
+  name: 'PlateTemplate',
   components: {
     WellGrid,
     Tag
   },
   methods: {
-    onClick() {
-      const tagInfo = {
-        objectId: this.plate.id,
-        objectClass: "PLATE",
-        tag: this.plateTag
-      }
-
-      this.$store.dispatch('plates/tagPlate', tagInfo)
-    },
-    addMeasurement() {
-      const plateMeasurement = {
-        plateId: this.plate,
-        measurementId: this.selectedMeas.id,
-        linkedBy: 'sberberovic',
-        linkedOn: new Date()
-      }
-      this.$store.dispatch('plates/addMeasurement', plateMeasurement)
-    },
-    deletePlate() {
-      this.$store.dispatch('plates/deletePlate', this.plate)
+    deletePlateTemplate() {
+      this.$store.dispatch('templates/deletePlateTemplate', this.plateTemplate.id)
     }
   },
   setup() {
     const store = useStore()
+    const route = useRoute()
 
-    const plateId = 1;
-    store.dispatch('plates/loadById', plateId);
+    const plateTemplateId = parseInt(route.params.id);
 
-    const plate = computed(() => store.getters['plates/getCurrentPlate']());
-    store.dispatch('plates/loadPlateTags', plateId)
-    const experiment = computed(() => store.getters['experiments/getById'](plate.value.experimentId));
-    const project = computed(() => store.getters['projects/getById'](experiment.value.projectId));
+    store.dispatch('templates/loadById', plateTemplateId);
 
-    // Once the plate has loaded, make sure the parent experiment gets loaded too.
-    watch(plate, (plate) => {
-      if (!store.getters['experiments/isLoaded'](plate.experimentId)) {
-        store.dispatch('experiments/loadById', plate.experimentId);
-      }
-    })
+    const plateTemplate = computed(() => store.getters['templates/getCurrentPlateTemplate']());
+    //store.dispatch('plates/loadPlateTags', plateTemplateId)
 
     return {
-      plate,
-      experiment,
-      project,
+      plateTemplate,
       propertyColumns,
       activeTab: ref('plate_layout'),
       layout: WellGrid.GRID_TYPE_LAYOUT
@@ -179,7 +179,7 @@ export default {
     return {
       plateTag: ref(""),
       prompt: ref(false),
-      plateName: ref(""),
+      plateTemplateName: ref(""),
       deletedialog: ref(false),
       editdialog: false,
     }
