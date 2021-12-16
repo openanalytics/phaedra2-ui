ff7404d3f05cacd0bda3e2a9dd7780c3a3f34fc2
smarien <simon.marien@openanalytics.eu>
2021-12-16 10:29:18 +0100

    Add all plate templates api calls.

--- src/api/templates.js
+++ src/api/templates.js
@@ -14,5 +14,53 @@ export default {
                 console.log(error);
             });
         return result;
+    },
+    async getAllPlateTemplates() {
+        let result = null
+        await axios.get(apiURL + '/plate-templates')
+            .then(response => {
+                if (response.status === 200)
+                    result = response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        return result;
+    },
+    async deletePlateTemplate(id) {
+        let result = null;
+        await axios.delete(apiURL + '/plate-template/' + id)
+            .then(response => {
+                if (response.status === 200)
+                    result = response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        return result;
+    },
+    async editPlateTemplate(args) {
+        let result = null;
+        await axios.put(apiURL + '/plate-template', args)
+            .then(response => {
+                if (response.status === 200)
+                    result = response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        return result;
+    },
+    async createPlateTemplate(newPlateTemplate) {
+        let result = null;
+        await axios.post(apiURL + '/plate-template', newPlateTemplate)
+            .then(response => {
+                if (response.status === 201)
+                    result = response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        return result;
     }
 }
