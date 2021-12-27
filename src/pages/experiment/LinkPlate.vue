00a75a6ac123537e75aa856380a2e523057774ab
smarien <simon.marien@openanalytics.eu>
2021-12-27 14:24:22 +0100

    Add Source dropdown and link status icon

--- src/pages/experiment/LinkPlate.vue
+++ src/pages/experiment/LinkPlate.vue
@@ -11,20 +11,27 @@
             <span>By linking plates with the system they were defined in, substance and control information can be retrieved and applied to the plate(s).</span><br><br>
           </div>
         </div>
+        <div class="row text-h6 items-center q-px-sm oa-section-title full-width">
+          <q-icon name="playlist_add" class="on-left"/>
+          Link with
+        </div>
+        <q-select class="full-width" v-model="source" label="Source:" :options="sourceOptions"></q-select>
+        <br>
         <div class="row text-h6 items-center q-px-sm oa-section-title">
-          <q-icon name="view_module" class="on-left"/>Plates
+          <q-icon name="view_module" class="on-left"/>
+          Plates
         </div>
         <q-table
             table-header-class="text-white bg-primary"
             :rows="plates"
-            :columns="columns"
+            :columns="plateColumns"
             :pagination="{ rowsPerPage: 10 }"
             class="full-width"
             square
             flat
             dense
             selection="multiple"
-            v-model:selected="selected"
+            v-model:selected="selectedPlates"
         >
           <template v-slot:header="props">
             <q-tr :props="props" class="text-white bg-primary">
@@ -36,12 +43,64 @@
           <template v-slot:body="props">
             <q-tr class="cursor-pointer" :props="props" @click="props.selected = !props.selected">
               <q-td v-for="col in props.cols" :key="col.name" :props="props">
-                <span v-if="!(col.name === 'select')">{{ col.value }}</span>
+                <span v-if="(col.name === 'layout')">{{ props.row.rows }} x {{ props.row.columns }}</span>
+                <q-icon v-else-if="col.name === 'status' && props.row.linkStatus==='LINKED'" name="check_circle" color="positive"/>
+                <q-icon v-else-if="col.name === 'status' && props.row.linkStatus==='NOT_LINKED'" name="cancel" color="negative"/>
+                <span v-else>{{ col.value }}</span>
+              </q-td>
+            </q-tr>
+          </template>
+        </q-table>
+        <span v-if="!checkPlateDimensions()" class="text-accent">The selected plates have different dimensions.</span><br>
+
+        <div class="row justify-between">
+          <div class="row text-h6 items-center q-px-sm oa-section-title">
+            <q-icon name="border_outer" class="on-left"/>
+            Templates
+          </div>
+          <div class="action-button">
+            <q-btn v-if="!selectedTemplate.length>0" size="sm" color="primary" icon="remove_red_eye" label="Quick view" disable/>
+            <q-btn v-if="selectedTemplate.length>0" size="sm" color="primary" icon="remove_red_eye" label="Quick view" @click="openQuickView"/>
+          </div>
+        </div>
+        <q-table
+            table-header-class="text-white bg-primary"
+            :rows="allTemplates"
+            :columns="templateColumns"
+            :pagination="{ rowsPerPage: 10 }"
+            class="full-width"
+            square
+            flat
+            dense
+            selection="single"
+            v-model:selected="selectedTemplate"
+            :filter="selectedPlates"
+            :filter-method="filterMethod"
+        >
+          <template v-slot:header="props">
+            <q-tr :props="props" class="text-white bg-primary">
+              <q-th v-for="col in props.cols" :key="col.name" :props="props">
+                {{ col.label }}
+              </q-th>
+            </q-tr>
+          </template>
+          <template v-slot:body="props">
+            <q-tr class="cursor-pointer" :props="props" @click="props.selected = !props.selected">
+              <q-td v-for="col in props.cols" :key="col.name" :props="props">
+                <span v-if="!(col.name === 'select' || col.name === 'layout')">{{ col.value }}</span>
+                <span v-if="(col.name === 'layout')">{{ props.row.rows }} x {{ props.row.columns }}</span>
               </q-td>
             </q-tr>
           </template>
         </q-table>
       </q-card-section>
+      <q-card-actions align="right" class="text-primary">
+        <q-btn flat label="Cancel" v-close-popup @click="$emit('update:show',false)"/>
+        <q-btn flat label="Link" disable v-if="!selectedPlates.length>0||!checkPlateDimensions()"
+               v-close-popup/>
+        <q-btn flat label="Link" v-if="selectedPlates.length>0&&checkPlateDimensions()"
+               @click="linkPlate" v-close-popup/>
+      </q-card-actions>
     </q-card>
   </q-dialog>
 </template>
@@ -59,20 +118,60 @@ export default {
 
     const experimentId = parseInt(route.params.id);
     const plates = computed(() => store.getters['plates/getByExperimentId'](experimentId))
-    const selected = ref([])
+    const allTemplates = store.getters['templates/getAll']()
+
+    const selectedPlates = ref([])
+    const selectedTemplate = ref([])
+    const source = ref('Layout Template')
+    const sourceOptions = ['Layout Template']
     return {
       props,
       plates,
-      selected,
-      columns: [
+      selectedPlates,
+      allTemplates,
+      selectedTemplate,
+      source,
+      sourceOptions,
+      plateColumns: [
         {name: 'sequence', align: 'left', label: 'Sequence', field: 'sequence', sortable: true},
         {name: 'barcode', align: 'left', label: 'Barcode', field: 'barcode', sortable: true},
         {name: 'description', align: 'left', label: 'Description', field: 'description', sortable: true},
-        {name: 'status', align: 'left', label: 'Status', field: 'linkStatus', sortable: true}
-      ]
+        {name: 'layout', align: 'center', label: 'Layout', field: 'layout', sortable: true},
+        {name: 'status', align: 'center', label: 'Link status', field: 'linkStatus', sortable: true}
+      ],
+      templateColumns: [
+        {name: 'name', align: 'left', label: 'Name', field: 'name', sortable: true},
+        {name: 'description', align: 'left', label: 'Description', field: 'description', sortable: true},
+        {name: 'layout', align: 'center', label: 'Layout', field: 'layout', sortable: true}
+      ],
+      filter: ref('')
     }
   },
   props: ['show'],
-  emits: ['update:show']
+  emits: ['update:show'],
+  methods: {
+    linkPlate() {
+
+    },
+    checkPlateDimensions() {
+      //Count occurences of rows and columns in selected plates
+      const countRows = this.selectedPlates.map(p => p.rows).filter(this.onlyUnique).length
+      const countColumns = this.selectedPlates.map(p => p.rows).filter(this.onlyUnique).length
+      if(countRows>1||countColumns>1)return false
+      return true
+    },
+    openQuickView() {
+
+    },
+    onlyUnique(value, index, self) {
+      return self.indexOf(value) === index;
+    },
+    filterMethod(cols, terms) {
+      if(terms.length===0)return cols
+      if (!this.checkPlateDimensions()) return []
+      const correctRows = cols.filter(row => row.rows===terms[0].rows)
+      return correctRows.filter(row => row.columns===terms[0].columns)
+    }
+  }
 }
 </script>
