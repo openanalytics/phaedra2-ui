9daef5e84bd45f0f1a2292ec85e8860f03161fca
Frederick Michielssen <fmichielssen@openanalytics.eu>
2022-09-07 14:31:51 +0200

    Added user names list and UserChip

---
+++ src/components/widgets/UserChip.vue
@@ -0,0 +1,19 @@
+<template>
+    <q-chip dense>
+        <q-avatar icon="account_circle" />
+        {{ userName }}
+    </q-chip>
+</template>
+
+<script setup>
+    import {computed} from "vue";
+    import {useStore} from 'vuex'
+
+    const props = defineProps({
+        id: String
+    });
+
+    const store = useStore();
+    const userName = computed(() => store.getters['userinfo/getUserName'](props.id));
+    if (!store.getters['userinfo/areUserNamesLoaded']()) store.dispatch('userinfo/loadUserNames');
+</script>
