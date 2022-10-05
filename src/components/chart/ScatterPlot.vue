c230969aebb55fd610b1f820820f98dcc98f81a2
smarien <simon.marien@openanalytics.eu>
2022-10-05 16:36:56 +0200

    Fix width + axis categories

--- src/components/chart/ScatterPlot.vue
+++ src/components/chart/ScatterPlot.vue
@@ -6,17 +6,39 @@
 import PlotLy from 'plotly.js-dist'
 import {onMounted, watch, watchEffect} from "vue";
 
-const props = defineProps(['show', 'data']);
+const props = defineProps(['data', 'width']);
+
+let layout = {
+  margin: {t:5, l:40, r:5, b:20},
+  "xaxis": {
+    "autorange": true,
+  },
+  "yaxis": {
+    "autorange": true,
+  }
+}
+
+const getAxisType = (data) => {
+  if (data.every(d => typeof d === 'number')) return 'linear';
+  return 'category';
+}
 
 // Wait for the DOM to be ready
 onMounted(() => {
-  PlotLy.newPlot('plot', [props.data]);
+  const width = document.getElementById('plot').parentElement.clientWidth*3;
+  console.log(width);
+  layout.width = width;
+  layout.xaxis.type = getAxisType(props.data.x);
+  layout.yaxis.type = getAxisType(props.data.y);
+  PlotLy.newPlot('plot', [props.data], layout);
 });
 
 // Call method when data changes
 watch(props.data, () => {
   console.log('data changed');
-  PlotLy.newPlot('plot', [props.data]);
+  layout.xaxis.type = getAxisType(props.data.x);
+  layout.yaxis.type = getAxisType(props.data.y);
+  PlotLy.newPlot('plot', [props.data],layout);
 });
 
 
