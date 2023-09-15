059dd75a55117c027f852c8a165e6d72f795afdf
sberberovic <sasa.berberovic@openanalytics.eu>
2023-09-15 13:28:47 +0200

    update: integrate with pinia store(s)

--- src/components/table/TableConfig.vue
+++ src/components/table/TableConfig.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-dialog v-model="props.show" persistent>
+  <q-dialog v-model="showDialog" persistent>
     <q-card style="min-width: 40vw">
       <q-card-section class="row text-h6 items-center full-width q-pa-sm bg-primary text-secondary">
         <q-avatar icon="settings" color="primary" text-color="white"/>
@@ -67,98 +67,92 @@
   </q-dialog>
 </template>
 
-<script>
+<script setup>
 
-import {watch, reactive} from "vue";
+import {watch, reactive, ref, computed} from "vue";
 
-export default {
-  /**
-   * Component to toggle visibility of table columns
-   * @props.show boolean to open and close configuration dialog
-   * @props.columns list with all columns
-   * @props.visibleColumns list with visible table columns
-   */
-  name: 'TableConfig',
-  methods: {
-    moveUp(row) {
-      const index = this.colslist.list.indexOf(row)
-      if (index === 0) return
-      const length = this.colslist.list.length
-      let orderedList = []
-      for (let i = 0; i < length - 1; i++) {
-        if (i === index - 1) {
-          const temp = this.colslist.list.shift()
-          orderedList.push(this.colslist.list.shift())
-          orderedList.push(temp)
-          continue
-        }
-        orderedList.push(this.colslist.list.shift())
-      }
-      this.colslist.list = orderedList
-    },
-    moveDown(row) {
-      const index = this.colslist.list.indexOf(row)
-      const length = this.colslist.list.length
-      if (index === length - 1) return
-      let orderedList = []
-      for (let i = 0; i < length - 1; i++) {
-        if (i === index) {
-          const temp = this.colslist.list.shift()
-          orderedList.push(this.colslist.list.shift())
-          orderedList.push(temp)
-          continue
-        }
-        orderedList.push(this.colslist.list.shift())
-      }
-      this.colslist.list = orderedList
-    },
-    update() {
-      let newOrder = []
-      let tempList = this.colslist.list.slice()
-      let tempColumns = this.columns.slice()
-      while (tempList.length > 0) {
-        const shift = tempList.shift()
-        newOrder.push(tempColumns.find(obj => obj.name === shift.column))
-      }
-      this.$emit('update:columns', newOrder)
-      this.$emit('update:visibleColumns', this.visible.list);
-      this.$emit('update:show', false)
-    }
-  },
-  setup(props) {
+const props = defineProps(['show', 'columns'])
+const emits = defineEmits(['update:visibleColumns', 'update:columns', 'update:show'])
 
-    const dialogColumns = [
-      {name: 'select', align: 'center', label: 'Visible', field: 'select', sortable: false},
-      {name: 'column', align: 'left', label: 'Column', field: 'column', sortable: true},
-      {name: 'dataType', align: 'left', label: 'Data Type', field: 'dataType', sortable: true},
-      {name: 'description', align: 'left', label: 'Description', field: 'description', sortable: true}
-    ]
-    //Make props reactive so they get updated in the table
-    let visible = reactive({list: props.columns?.map(a => a.name)})
-    let colslist = reactive({list: props.columns?.map(a => ({column: a.name, dataType: (Math.random() + 1).toString(36).substring(7), description: (Math.random() + 1).toString(36).substring(2)}))})
-    //Watch for props change to update lists
-    watch(props.columns, (cols) => {
-      visible.list = cols.map(a => a.name)
-      colslist.list = cols.map(a => ({
-        column: a.name,
-        dataType: (Math.random() + 1).toString(36).substring(7),
-        description: (Math.random() + 1).toString(36).substring(2)
-      }))
-    })
-    return {
-      props,
-      dialogColumns,
-      colslist,
-      visible
+const showDialog = computed({
+  get: () => props.show,
+  set: (v) => emits('update:show', v)
+})
+
+const configdialog = ref(props.show)
+const selected = ref([])
+
+//Make props reactive so they get updated in the table
+let visible = reactive({list: props.columns?.map(a => a.name)})
+let colslist = reactive({
+  list: props.columns?.map(a => ({
+    column: a.name,
+    dataType: (Math.random() + 1).toString(36).substring(7),
+    description: (Math.random() + 1).toString(36).substring(2)
+  }))
+})
+
+const moveUp = (row) => {
+  const index = colslist.list.indexOf(row)
+  if (index === 0) return
+  const length = colslist.list.length
+  let orderedList = []
+  for (let i = 0; i < length - 1; i++) {
+    if (i === index - 1) {
+      const temp = colslist.list.shift()
+      orderedList.push(colslist.list.shift())
+      orderedList.push(temp)
+      continue
     }
-  },
-  data() {
-    return {
-      configdialog: this.props.show,
-      selected: []
-    }
-  },
-  props: ['show', 'columns'],
-  emits: ['update:visibleColumns', 'update:columns', 'update:show']
+    orderedList.push(colslist.list.shift())
+  }
+  colslist.list = orderedList
 }
+
+const moveDown = (row) => {
+  const index = colslist.list.indexOf(row)
+  const length = colslist.list.length
+  if (index === length - 1) return
+  let orderedList = []
+  for (let i = 0; i < length - 1; i++) {
+    if (i === index) {
+      const temp = colslist.list.shift()
+      orderedList.push(colslist.list.shift())
+      orderedList.push(temp)
+      continue
+    }
+    orderedList.push(colslist.list.shift())
+  }
+  colslist.list = orderedList
+}
+const update = () => {
+  let newOrder = []
+  let tempList = colslist.list.slice()
+  let tempColumns = columns.slice()
+  while (tempList.length > 0) {
+    const shift = tempList.shift()
+    newOrder.push(tempColumns.find(obj => obj.name === shift.column))
+  }
+  this.$emit('update:columns', newOrder)
+  this.$emit('update:visibleColumns', this.visible.list);
+  this.$emit('update:show', false)
+}
+
+const dialogColumns = [
+  {name: 'select', align: 'center', label: 'Visible', field: 'select', sortable: false},
+  {name: 'column', align: 'left', label: 'Column', field: 'column', sortable: true},
+  {name: 'dataType', align: 'left', label: 'Data Type', field: 'dataType', sortable: true},
+  {name: 'description', align: 'left', label: 'Description', field: 'description', sortable: true}
+]
+
+//Watch for props change to update lists
+watch(props.columns, (cols) => {
+  visible.list = cols.map(a => a.name)
+  colslist.list = cols.map(a => ({
+    column: a.name,
+    dataType: (Math.random() + 1).toString(36).substring(7),
+    description: (Math.random() + 1).toString(36).substring(2)
+  }))
+})
+
 </script>
