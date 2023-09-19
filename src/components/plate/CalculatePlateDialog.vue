999a5816d682303c3cf1dca707334137805a2968
sberberovic <sasa.berberovic@openanalytics.eu>
2023-09-19 07:31:58 +0200

    update: update calculate plate dialog with pinia store

--- src/components/plate/CalculatePlateDialog.vue
+++ src/components/plate/CalculatePlateDialog.vue
@@ -5,14 +5,14 @@
                 <q-avatar icon="calculate" color="primary" text-color="white"/>
                 Calculate Plate
             </q-card-section>
-            
+
             <q-card-section v-if="activeMeasurement && checkDimensions()">
                 <div class="q-pb-sm">
                     Select the protocol to use for calculation:
                 </div>
                 <protocol-selectable-list v-model:selected="selected"></protocol-selectable-list>
             </q-card-section>
-            
+
             <q-card-section v-if="!activeMeasurement">
                 <q-icon name="warning" color="negative" class="on-left"/>
                 <span class="text-accent text-weight-bold">Cannot calculate: this plate has no active measurement.</span>
@@ -21,7 +21,7 @@
                 <q-icon name="warning" color="negative" class="on-left"/>
                 <span class="text-accent text-weight-bold">Cannot calculate: the plate and measurement dimensions are different.</span>
             </q-card-section>
-            
+
             <q-card-actions align="right" class="text-primary">
                 <q-btn flat label="Cancel" v-close-popup/>
                 <q-btn label="Calculate" color="primary" :disable="!activeMeasurement || selected.length === 0 || !checkDimensions()" @click="calculatePlate" v-close-popup/>
@@ -35,13 +35,16 @@
 import {computed,ref,watch} from "vue";
 import {useStore} from "vuex";
 import ProtocolSelectableList from "@/components/protocol/ProtocolSelectableList";
+import {useCalcStore} from "@/stores/calculations";
+import projectsGraphQlAPI from "@/api/graphql/projects"
 
-const props = defineProps(['show', 'plateId']);
+const props = defineProps(['show', 'plate']);
 const emits = defineEmits(['update:show']);
 
 const store = useStore();
-const plate = computed(() => store.getters['plates/getById'](props.plateId));
-const activeMeasurement = computed(() => store.getters['measurements/getActivePlateMeasurement'](props.plateId));
+const calculationStore = useCalcStore()
+const plate = computed(() => props.plate);
+const activeMeasurement = projectsGraphQlAPI.activePlateMeasurement(plate.value.id)
 
 const showDialog = computed({
     get: () => props.show,
@@ -49,22 +52,22 @@ const showDialog = computed({
 });
 
 watch(() => props.show, (v) => {
-    if (v === true) store.dispatch('measurements/loadByPlateId', props.plateId);
+  if (v) store.dispatch('measurements/loadByPlateId', plate.value.id)
 });
 
-watch(() => props.plateId, (v) => {
-    store.dispatch('plates/loadById', v);
+watch(() => props.plate, (v) => {
+    store.dispatch('plates/loadById', v.id);
 });
 
 const selected = ref([]);
 
 const calculatePlate = () => {
-    store.dispatch('calculations/startCalculation', {
-        protocolId:  selected.value[0].id,
-        plateId: props.plateId,
-        measId: activeMeasurement.value.measurementId
-    });
-    emits('update:show', false);
+  calculationStore.startCalculation({
+    protocolId: selected.value[0].id,
+    plateId: Number.parseInt(plate.value.id),
+    measId: activeMeasurement.value.measurementId
+  })
+  emits('update:show', false);
 };
 
 const checkDimensions = () => {
