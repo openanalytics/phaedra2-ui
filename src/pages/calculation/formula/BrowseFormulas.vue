7d6dfddbad7c9b7e4a692bcb9790d11f1db10deb
sberberovic <sasa.berberovic@openanalytics.eu>
2024-01-10 14:44:36 +0100

    update: add filter by column feature

--- src/pages/calculation/formula/BrowseFormulas.vue
+++ src/pages/calculation/formula/BrowseFormulas.vue
@@ -1,80 +1,86 @@
 <template>
-    <q-breadcrumbs class="oa-breadcrumb">
-        <q-breadcrumbs-el icon="home" :to="{ name: 'dashboard'}" />
-        <q-breadcrumbs-el :label="'Formulas'" icon="list"/>
-    </q-breadcrumbs>
+  <q-breadcrumbs class="oa-breadcrumb">
+    <q-breadcrumbs-el icon="home" :to="{ name: 'dashboard'}"/>
+    <q-breadcrumbs-el :label="'Formulas'" icon="list"/>
+  </q-breadcrumbs>
 
-    <q-page class="oa-root-div">
-        <div class="q-pa-sm">
-            <oa-section title="Calculation Formulas" icon="functions">
-                <q-table
-                table-header-class="text-grey"
-                class="full-width"
-                flat dense
-                :rows="formulas"
-                :columns="columns"
-                row-key="id"
-                :pagination="{ rowsPerPage: 20, sortBy: 'name' }"
-                :filter="filter"
-                :filter-method="filterMethod"
-                :loading="loading"
-                >
-                <template v-slot:top-left>
-                    <q-btn size="sm" color="primary" icon="add" label="New Formula..." @click="createNewFormula"/>
-                </template>
-                <template v-slot:top-right>
-                    <q-toggle v-model="showDeprecated" dense label="Show Deprecated" size="sm" class="on-left"/>
-                    <q-input outlined dense debounce="300" v-model="filter" placeholder="Search">
-                        <template v-slot:append>
-                            <q-icon name="search"/>
-                        </template>
-                    </q-input>
-                </template>
-                <template v-slot:body-cell-name="props">
-                    <q-td :props="props" class="cursor-pointer" @click="selectFormula(props.row)">
-                        <q-chip square dense>
-                            {{ props.row.name }}
-                        </q-chip>
-                    </q-td>
-                </template>
-                <template v-slot:body-cell-createdBy="props">
-                    <q-td :props="props">
-                        <UserChip :id="props.row.createdBy" />
-                    </q-td>
-                </template>
-                <template v-slot:body-cell-menu="props">
-                    <q-td :props="props">
-                        <div class="row items-center cursor-pointer">
-                            <q-btn flat round icon="more_horiz" size="sm" style="border-radius: 50%;">
-                                <q-menu fit>
-                                    <q-list dense style="min-width: 100px">
-                                        <q-item clickable :to="'/calc/formula/' + props.row.id">
-                                            <q-item-section>
-                                                <q-icon name="info"/>
-                                            </q-item-section>
-                                            <q-item-section>Details</q-item-section>
-                                        </q-item>
-                                        <q-item clickable @click="deleteFormula(props.row)">
-                                            <q-item-section>
-                                                <q-icon name="delete"/>
-                                            </q-item-section>
-                                            <q-item-section>Delete</q-item-section>
-                                        </q-item>
-                                    </q-list>
-                                </q-menu>
-                            </q-btn>
-                        </div>
-                    </q-td>
-                </template>
-            </q-table>
-        </oa-section>
+  <q-page class="oa-root-div">
+    <div class="q-pa-sm">
+      <oa-section title="Calculation Formulas" icon="functions">
+        <q-table
+            class="full-width"
+            table-header-class="text-grey"
+            :rows="filteredFormulas"
+            :columns="columns"
+            :visible-columns="visibleColumns"
+            row-key="id"
+            column-key="name"
+            :pagination="{ rowsPerPage: 20, sortBy: 'name' }"
+            :loading="loading"
+            flat square dense
+        >
+          <template v-slot:top-left>
+            <q-btn size="sm" color="primary" icon="add" label="New Formula..." @click="createNewFormula"/>
+          </template>
+          <template v-slot:header-cell="props">
+            <q-td :props="props" class="row-cols-1">
+              <div>
+                <q-input v-if="props.col.name !== 'menu'" v-model="columnFilters[props.col.name]"
+                         :label="props.col.label"
+                         @update:model-value="handleColumnFilter(props.col.name)"
+                         dense>
+                  <template v-slot:append>
+                    <q-icon size="xs" name="search"/>
+                  </template>
+                </q-input>
+              </div>
+            </q-td>
+          </template>
+          <template v-slot:body-cell-name="props">
+            <q-td :props="props" class="cursor-pointer" @click="selectFormula(props.row)">
+              <q-chip square dense>
+                {{ props.row.name }}
+              </q-chip>
+            </q-td>
+          </template>
+          <template v-slot:body-cell-createdBy="props">
+            <q-td :props="props">
+              <UserChip :id="props.row.createdBy"/>
+            </q-td>
+          </template>
+          <template v-slot:body-cell-menu="props">
+            <q-td :props="props">
+              <div class="row items-center cursor-pointer">
+                <q-btn flat round icon="more_horiz" size="sm" style="border-radius: 50%;">
+                  <q-menu fit>
+                    <q-list dense style="min-width: 100px">
+                      <q-item clickable :to="'/calc/formula/' + props.row.id">
+                        <q-item-section>
+                          <q-icon name="info"/>
+                        </q-item-section>
+                        <q-item-section>Details</q-item-section>
+                      </q-item>
+                      <q-item clickable @click="deleteFormula(props.row)">
+                        <q-item-section>
+                          <q-icon name="delete"/>
+                        </q-item-section>
+                        <q-item-section>Delete</q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu>
+                </q-btn>
+              </div>
+            </q-td>
+          </template>
+        </q-table>
+      </oa-section>
     </div>
-</q-page>
-<DeleteFormulaDialog :ref="el => deleteDialog = el"/>
+  </q-page>
+  <DeleteFormulaDialog :ref="el => deleteDialog = el"/>
 </template>
 
 <script setup>
-import {computed, ref, watchEffect} from 'vue'
+import {computed, ref, watch, watchEffect} from 'vue'
 import {useStore} from 'vuex'
 import {useRouter} from 'vue-router'
 import FormatUtils from "@/lib/FormatUtils.js"
@@ -105,16 +111,20 @@ const columns = [
     {name: 'menu', align: 'left', field: 'menu', sortable: false}
 ];
 
-const filter = ref('');
-const filterMethod = function (rows, term) {
-    return rows.filter(row => {
-        for (const field in row) {
-            const stringValue = String(row[field]);
-            if (stringValue && stringValue.toLowerCase().includes(term.toLowerCase())) return true;
-        }
-        return false;
-    })
-};
+const filteredFormulas = ref([])
+const columnFilters = ref({})
+const visibleColumns = ref([])
+
+// const filter = ref('');
+// const filterMethod = function (rows, term) {
+//     return rows.filter(row => {
+//         for (const field in row) {
+//             const stringValue = String(row[field]);
+//             if (stringValue && stringValue.toLowerCase().includes(term.toLowerCase())) return true;
+//         }
+//         return false;
+//     })
+// };
 
 const router = useRouter();
 
@@ -129,4 +139,22 @@ const deleteDialog = ref(null);
 const deleteFormula = (row) => {
     deleteDialog.value.openDialog(row);
 }
+
+const updateVisibleColumns = (columns) => {
+  visibleColumns.value = [...columns]
+}
+
+const handleColumnFilter = (columnName) => {
+  filteredFormulas.value = formulas.value.filter(row => String(row[columnName]).includes(columnFilters.value[columnName]))
+}
+
+watch(formulas, () => {
+  visibleColumns.value = [...columns.map(a => a.name)];
+  filteredFormulas.value = [...formulas.value.map(r => r)]
+  loading.value = false
+
+  columns.forEach(col => {
+    columnFilters.value[col.name] = ref(null)
+  })
+})
 </script>
