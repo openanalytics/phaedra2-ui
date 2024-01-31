271c644fc2f753d31ea991b7b7a426eb418643c7
Frederick Michielssen <fmichielssen@openanalytics.eu>
2024-01-31 17:56:37 +0100

    Fix userInfo parse issue when no roles are present in claims

--- src/api/userinfo.js
+++ src/api/userinfo.js
@@ -23,10 +23,10 @@ export default {
         try {
             const response = mockData.enabled ? mockData.userInfo : await axios.get(apiURL + '/userinfo');
             const userInfo = response.data;
-            userInfo.teams = userInfo.claims.realm_access.roles
+            userInfo.teams = userInfo?.claims?.realm_access?.roles
                 .filter(role => role.startsWith("phaedra2-team-"))
                 .map(role => role.substring("phaedra2-team-".length))
-                .sort();
+                .sort() || [];
             userInfo.admin = userInfo.claims.realm_access.roles.includes("phaedra2-admin");
 
             return userInfo;
