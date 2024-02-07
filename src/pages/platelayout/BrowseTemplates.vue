67c4e548c9cc1464420d5e0ab2f724ac899323a5
sberberovic <sasa.berberovic@openanalytics.eu>
2024-02-07 14:12:07 +0100

    update: make table column filter fields compacter

--- src/pages/platelayout/BrowseTemplates.vue
+++ src/pages/platelayout/BrowseTemplates.vue
@@ -1,65 +1,3 @@
-<template>
-  <q-breadcrumbs class="oa-breadcrumb">
-    <q-breadcrumbs-el icon="home" :to="{ name: 'dashboard'}"/>
-    <q-breadcrumbs-el icon="list" :label="'Templates'"/>
-  </q-breadcrumbs>
-
-  <q-page class="oa-root-div">
-    <div class="q-pa-sm">
-      <oa-section title="Plate Layout Templates" icon="border_outer">
-        <q-table
-            table-header-class="text-grey"
-            class="full-width"
-            :rows="filteredTemplates"
-            :columns="columns"
-            :visible-columns="visibleColumns"
-            row-key="id"
-            column-key="name"
-            :loading="loading"
-            :pagination="{ rowsPerPage: 20, sortBy: 'name' }"
-            @row-click="selectTemplate"
-            separator="cell"
-            flat square dense
-        >
-          <template v-slot:top-left>
-            <router-link :to="{ name: 'newPlateTemplate' }" class="nav-link">
-              <q-btn size="sm" icon="add" class="oa-button" label="New Plate Template"/>
-            </router-link>
-          </template>
-          <template v-slot:header="props">
-            <q-tr :props="props">
-              <q-th v-for="col in props.cols" :key="col.name" :props="props">
-                {{col.label}}
-              </q-th>
-            </q-tr>
-            <q-tr :props="props">
-              <q-th v-for="col in props.cols" :key="col.name">
-                <q-input v-if="col.name != 'menu'" v-model="columnFilters[col.name]"
-                         @update:model-value="handleColumnFilter(col.name)"
-                         dense>
-                  <template v-slot:append>
-                    <q-icon size="xs" name="search"/>
-                  </template>
-                </q-input>
-              </q-th>
-            </q-tr>
-          </template>
-          <template v-slot:body-cell-tags="props">
-            <q-td :props="props">
-              <q-badge v-for="tag in props.row.tags" :key="tag" color="green">{{ tag }}</q-badge>
-            </q-td>
-          </template>
-          <template v-slot:body-cell-createdBy="props">
-            <q-td :props="props">
-              <UserChip :id="props.row.createdBy"/>
-            </q-td>
-          </template>
-        </q-table>
-      </oa-section>
-    </div>
-  </q-page>
-</template>
-
 <script setup>
 import {onMounted, ref, watch} from 'vue'
 import {useStore} from 'vuex'
@@ -123,3 +61,73 @@ watch(templates, () => {
   })
 })
 </script>
+
+<template>
+  <q-breadcrumbs class="oa-breadcrumb">
+    <q-breadcrumbs-el icon="home" :to="{ name: 'dashboard'}"/>
+    <q-breadcrumbs-el icon="list" :label="'Templates'"/>
+  </q-breadcrumbs>
+
+  <q-page class="oa-root-div">
+    <div class="q-pa-sm">
+      <oa-section title="Plate Layout Templates" icon="border_outer">
+        <q-table
+            table-header-class="text-grey"
+            class="full-width"
+            :rows="filteredTemplates"
+            :columns="columns"
+            :visible-columns="visibleColumns"
+            row-key="id"
+            column-key="name"
+            :loading="loading"
+            :pagination="{ rowsPerPage: 20, sortBy: 'name' }"
+            @row-click="selectTemplate"
+            separator="cell"
+            flat square dense
+        >
+          <template v-slot:top-left>
+            <router-link :to="{ name: 'newPlateTemplate' }" class="nav-link">
+              <q-btn size="sm" icon="add" class="oa-button" label="New Plate Template"/>
+            </router-link>
+          </template>
+          <template v-slot:header="props">
+            <q-tr :props="props">
+              <q-th v-for="col in props.cols" :key="col.name" :props="props">
+                {{col.label}}
+              </q-th>
+            </q-tr>
+            <q-tr :props="props">
+              <q-th v-for="col in props.cols" :key="col.name">
+                <q-input v-if="col.name != 'menu'" v-model="columnFilters[col.name]"
+                         @update:model-value="handleColumnFilter(col.name)"
+                         dense>
+                  <template v-slot:append>
+                    <q-icon size="xs" name="search"/>
+                  </template>
+                </q-input>
+              </q-th>
+            </q-tr>
+          </template>
+          <template v-slot:body-cell-tags="props">
+            <q-td :props="props">
+              <q-badge v-for="tag in props.row.tags" :key="tag" color="green">{{ tag }}</q-badge>
+            </q-td>
+          </template>
+          <template v-slot:body-cell-createdBy="props">
+            <q-td :props="props">
+              <UserChip :id="props.row.createdBy"/>
+            </q-td>
+          </template>
+        </q-table>
+      </oa-section>
+    </div>
+  </q-page>
+</template>
+
+<style scoped>
+:deep(.q-field__control),
+:deep(.q-field__append){
+  font-size: 12px;
+  height: 25px;
+}
+</style>
