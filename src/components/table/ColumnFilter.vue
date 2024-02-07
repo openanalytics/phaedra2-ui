f010c3f714952e139ceec95d6baabd6679afb9ac
sberberovic <sasa.berberovic@openanalytics.eu>
2024-02-07 19:20:35 +0100

    update: make table column filter fields compacter

--- src/components/table/ColumnFilter.vue
+++ src/components/table/ColumnFilter.vue
@@ -1,19 +1,21 @@
 <template>
-    <q-th style="padding: 0px 8px 0px 8px;">
-        <q-input v-model="model" dense borderless input-class="q-py-none">
-            <template v-slot:append>
-                <q-icon size="xs" name="search"/>
-            </template>
-        </q-input>
-    </q-th>
+  <q-th>
+    <q-input v-model="model" dense borderless input-class="q-py-none">
+      <template v-slot:append>
+        <q-icon size="xs" name="search"/>
+      </template>
+    </q-input>
+  </q-th>
 </template>
 
-<style>
-    .q-field__control, .q-field__append {
-        height: 32px !important;
-    }
+<style scoped>
+:deep(.q-field__control),
+:deep(.q-field__append) {
+  font-size: 12px;
+  height: 25px;
+}
 </style>
 
 <script setup>
-    const model = defineModel();
+const model = defineModel();
 </script>
