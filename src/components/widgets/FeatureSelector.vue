5e3db167120ef26c6c237fb8f62e433dec522d81
Frederick Michielssen <fmichielssen@openanalytics.eu>
2024-02-22 17:19:23 +0100

    Feature selector: added sorting and filtering

--- src/components/widgets/FeatureSelector.vue
+++ src/components/widgets/FeatureSelector.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="row no-wrap items-center">
-    <div>
+    <div class="col-auto">
       <q-btn-toggle v-model="selectedFeatureOption" :options="featureOptions" class="on-left"/>
     </div>
-    <div>
+    <div class="col-8">
       <q-select v-if="selectedFeatureOption === 'raw'"
                 v-model="selectedFeature"
                 :options="rawFeatureOptions"
+                options-dense
                 @update:model-value="value => handleRawFeatureSelection(value)"
                 @filter="filterRawOptions"
-                dense use-input/>
+                dense use-input hide-selected fill-input class="" />
       <q-select v-if="selectedFeatureOption === 'calculated'"
                 v-model="selectedFeature"
                 :options="calculatedFeatureOptions"
+                options-dense
                 option-value="featureId"
                 option-label="name"
                 @update:model-value="value => handleCalculatedFeatureSelection(value)"
                 @filter="filterCalculatedOptions"
-                dense use-input/>
+                dense use-input hide-selected fill-input />
     </div>
   </div>
 </template>
@@ -36,29 +38,37 @@ const featureOptions = [
   { label: 'Calculated Feature', value: 'calculated' }
 ]
 
-const allRawFeatures = computed(() => [...new Set(props.measurements.flatMap(m => m.wellColumns ?? []))])
+const allRawFeatures = computed(() => [...new Set(props.measurements.flatMap(m => m.wellColumns ?? []))].sort((f1, f2) => f1.localeCompare(f2)))
 const rawFeatureOptions = ref(allRawFeatures.value)
 
-const allCalculatedFeatures = computed(() => props.protocols.flatMap(protocol => protocol.features.map(feature => {
-  return {name: `[${protocol.name}] ${feature.name}`, protocolId: protocol.id, featureId: feature.id}
-})))
+const allCalculatedFeatures = computed(() => props.protocols.flatMap(protocol => protocol.features
+  .map(feature => {
+    return {name: `[${protocol.name}] ${feature.name}`, protocolId: protocol.id, featureId: feature.id}
+  })
+  .sort((f1, f2) => f1.name.localeCompare(f2.name))
+))
 const calculatedFeatureOptions = ref(allCalculatedFeatures.value)
 
 const selectedFeature = ref(null)
-onMounted(() => {
-  initSelectedFeature()
-})
+
+onMounted(() => initSelectedFeature());
+watch(selectedFeatureOption, () => initSelectedFeature())
+
+watch(allRawFeatures, () => {
+  rawFeatureOptions.value = allRawFeatures.value;
+  if (!selectedFeature.value) initSelectedFeature();
+});
+watch(allCalculatedFeatures, () => {
+  calculatedFeatureOptions.value = allCalculatedFeatures.value;
+});
 
 const initSelectedFeature = () => {
-  if (selectedFeatureOption.value === 'raw')
+  if (selectedFeatureOption.value === 'raw' && rawFeatureOptions.value?.length > 0) {
     handleRawFeatureSelection(rawFeatureOptions.value[0])
-  else
+  } else if (calculatedFeatureOptions.value?.length > 0) {
     handleCalculatedFeatureSelection(calculatedFeatureOptions.value[0])
+  }
 }
-
-watch(selectedFeatureOption, () => {
-  initSelectedFeature()
-})
 
 const handleRawFeatureSelection = (feature) => {
   selectedFeature.value = feature
@@ -73,7 +83,7 @@ const handleCalculatedFeatureSelection = (feature) => {
 const filterRawOptions = (filter, update) => {
   update(() => {
     rawFeatureOptions.value = allRawFeatures.value.filter(rfo => {
-      return rfo.includes(filter)
+      return rfo.toLowerCase().includes((filter || "").toLowerCase())
     })
   })
 }
@@ -81,7 +91,7 @@ const filterRawOptions = (filter, update) => {
 const filterCalculatedOptions = (filter, update) => {
   update(() => {
     calculatedFeatureOptions.value = allCalculatedFeatures.value.filter(cfo => {
-      return cfo.name.includes(filter)
+      return cfo.name.toLowerCase().includes((filter || "").toLowerCase())
     })
   })
 }
