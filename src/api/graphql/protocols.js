d9c1a2041bc4b7c82f3d86c70b475a4805a9c899
sberberovic <sasa.berberovic@openanalytics.eu>
2024-03-20 08:46:07 +0100

    update: add tags to the protocol list

--- src/api/graphql/protocols.js
+++ src/api/graphql/protocols.js
@@ -14,6 +14,7 @@ export default {
                     description
                     createdOn
                     createdBy
+                    tags
                 }
             }
         `
