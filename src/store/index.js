0da2b2557cb0e99c3a1dfc94e1e48e7983cf6071
sberberovic <sasa.berberovic@openanalytics.eu>
2024-03-20 10:11:31 +0100

    fix: revert previous changes to index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,19 @@
 import { createStore, createLogger } from 'vuex'
 
+import projects from './modules/projects'
+import experiments from './modules/experiments'
+import plates from './modules/plates'
+import wells from './modules/wells'
+import protocols from './modules/protocols'
+// import pipelines from './modules/pipelines'
+import features from './modules/features'
 import measurements from './modules/measurements'
 import resultdata from './modules/resultdata'
 import calculations from './modules/calculations'
 import datacapture from './modules/datacapture'
+import templates from './modules/templates'
 import metadata from './modules/metadata'
+// import userinfo from './modules/userinfo'
 import ui from './modules/ui'
 import curvedata from "./modules/curvedata";
 import charting from "./modules/charting";
@@ -13,11 +22,20 @@ const debug = process.env.NODE_ENV !== 'production'
 
 export default createStore({
   modules: {
+    projects,
+    experiments,
+    plates,
+    wells,
+    protocols,
+    // pipelines,
+    features,
     measurements,
     resultdata,
     calculations,
     datacapture,
+    templates,
     metadata,
+    // userinfo,
     ui,
     curvedata,
     charting
@@ -25,3 +43,4 @@ export default createStore({
   strict: debug,
   plugins: debug ? [createLogger()] : []
 })
+
