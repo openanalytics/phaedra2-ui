aa6003f806bd5209697b61685e10830bee1756cd
sberberovic <sasa.berberovic@openanalytics.eu>
2024-09-05 10:47:20 +0200

    update: set correct scatter marker symbol based on the well status

--- src/components/chart/Chart.vue
+++ src/components/chart/Chart.vue
@@ -91,7 +91,6 @@ const initSelectedValues = () => {
     } else {
       isPlotlyClick = false
     }
-
   })
 }
 
@@ -135,14 +134,29 @@ const handleChartUpdate = () => {
   const chartView = computed(() => uiStore.getChartView(props.chartId))
   if (chartView.value.type === 'scatter') {
     const scatterChartData = useScatterChartData()
-    scatterChartData.getChartData(uiStore.selectedPlate.id, selectedProtocol.value?.id, selectedXAxisOption.value.value, selectedXAxisOption.value.type, selectedYAxisOption.value.value, selectedYAxisOption.value.type, groupBy.value.value).then((scatterData) => {
+    scatterChartData.getChartData(uiStore.selectedPlate.id, selectedProtocol.value?.id,
+        selectedXAxisOption.value.value, selectedXAxisOption.value.type,
+        selectedYAxisOption.value.value, selectedYAxisOption.value.type, groupBy.value.value).then(
+        (scatterData) => {
       console.log("Scatter Chart Data: " + Object.keys(scatterData))
-      const traces = Object.keys(scatterData).map(groupByKey => {return {x: scatterData[groupByKey].xvalues, y: scatterData[groupByKey].yvalues, customdata: scatterData[groupByKey].customdata, mode: "markers", type: "scatter", name: groupByKey}})
+      const traces = Object.keys(scatterData).map(groupByKey => {
+        return {
+          type: "scatter",
+          x: scatterData[groupByKey].xvalues,
+          y: scatterData[groupByKey].yvalues,
+          customdata: scatterData[groupByKey].customdata,
+          mode: "markers",
+          marker: {
+            size: 12,
+            symbol: scatterData[groupByKey].markerSymbol
+          },
+          name: groupByKey
+        }
+      })
       traces.forEach(trace => {
-        trace['marker'] = {size: 12}
+        // trace['marker'] = {size: 12}
         if (trace.name === 'PC') trace.marker['color'] = 'rgb(33,186,69)'
         if (trace.name === 'NC') trace.marker['color'] = 'rgb(229,35,35)'
-
       })
       chartPlot.value = {
         data: traces,
