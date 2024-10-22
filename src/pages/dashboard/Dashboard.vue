48254dcd48a9075ed1194fc93edbc85aada36e62
kkulesa <katarzyna.kulesa@openanalytics.eu>
2024-10-22 12:36:06 +0000

    feature/33963-disconnect-dashboard-from-workbench (#57)
    just a small change to disconnect the dashboard from the paneStore (before, the dashboard was displaying the same content as the workbench)
    
    ![image](/attachments/02d9ef50-ba7c-4026-9e5d-ac607ed519bc)
    ![image](/attachments/ffc3a4fd-553a-4366-8238-f2eed6a3e82d)
    
    Reviewed-on: https://scm.openanalytics.eu/git/phaedra2-ui/pulls/57
    Co-authored-by: kkulesa <katarzyna.kulesa@openanalytics.eu>
    Co-committed-by: kkulesa <katarzyna.kulesa@openanalytics.eu>

--- src/pages/dashboard/Dashboard.vue
+++ src/pages/dashboard/Dashboard.vue
@@ -2,7 +2,7 @@
   <q-page class="oa-root-div">
     <PanesDashboard
       style="height: calc(100vh - 50px)"
-      :dynamicPanes="panesStore.dynamicPanes"
+      :dynamicPanes="panesMap.get('dashboard').value"
       class="dashboard"
       :splitKey="1"
     ></PanesDashboard>
@@ -10,10 +10,8 @@
 </template>
 
 <script setup>
-import { usePanesStore } from "@/stores/panes";
 import PanesDashboard from "@/components/splitpanes/PanesDashboard.vue";
-
-const panesStore = usePanesStore();
+import { panesMap } from "@/maps/panes/panesMap";
 </script>
 
 <style>
